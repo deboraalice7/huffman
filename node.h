c5be0285cda5d11ed6f513360d3dafa4cdcbba5f
LarissaMerida <32376211+LarissaMerida@users.noreply.github.com>
2017-11-06 15:53:36 -0300

    Update node.h

--- node.h
+++ node.h
@@ -18,7 +18,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
+/*
+ * Estrutura de nó
+ */
 typedef struct node{
 	void *item;
 	void  *freq;
@@ -26,12 +29,33 @@ typedef struct node{
 	struct node *left;
 	struct node *right;
 }Node;
+	
+/*
+ * Função: colocar frequência de cada caracter em um array
+ * Recebe: arquivo de entrada e ponteiro de array 
+ */
 void frequency(FILE *in, int (*freq)[]);
 
+/*
+ * Função: setar nó
+ * Recebe: nó, ponteiros de item e de frequência
+/* Retorna: endereço de nó
+ */
 void* set_node(Node *l, void* item, void *freq);
 
+	
+/*
+ * Função: adicionar no na lista
+ * Recebe: nó da cabeça da lista e o novo nó
+ * Retorna: endereço do nó da cabeça da lista
+ */
 void* add_node(Node *HEAD, Node *new_node);
+	
+/*
+ * Função: retornar o endereço do nó da cabeça da lista
+ */
 void* dequeue(Node *HEAD);
+	
 #ifdef __cplusplus
 }
 #endif
