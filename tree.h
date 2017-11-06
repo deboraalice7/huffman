a1c73707435bbaf271574d4c8cb4f5a891aca057
LarissaMerida <32376211+LarissaMerida@users.noreply.github.com>
2017-11-06 15:36:24 -0300

    Update tree.h

--- tree.h
+++ tree.h
@@ -18,15 +18,41 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+  
+/*
+ * Função imprimir árvore em pré-ordem
+ */
 void print_pre_order(Node *tree);
+  
+  /*
+   * Função: criar árvore
+   * Recebe: node, quantidade de no,  ponteiro de frequência, ponteiro de item
+   * Retorna: endereço da cabeça da árvore
+   */
 void* build_tree(Node *list, short int *q_node, int *freq, int *item);
 
+  
+  /*
+   * Função: escrever árvore em pré-ordem no arquivo
+   * Recebe: arquivo de saída e a cabeça da árvore
+   */
 void set_tree(FILE *out, Node *tree);
 
+  /*
+   * Função: Fazer árvore predefinida em arquivo
+   * Recebe: arquivo de entrada, tamanho da árvore e ponteiro de freq
+   * Retorna: cabeça da árvore
+   */
 void* rebuild_huffman_tree(FILE *in, short int *tree_size,  void *freq);
 
+  /*
+   * Função: inserir na hashtable os bits de cada caracter
+   * Recebe: no, ponteiro do tamanho da árvore, 
+   * observação: incrementa tamanho de árvore ao percorrê-la
+   * 
+   */
 void* TraverseTree(Node *HEAD, short int *tree_size, unsigned char *prefix, int position, Hashtable *ht);
+  
 #ifdef __cplusplus
 }
 #endif
