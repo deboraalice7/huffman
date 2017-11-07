cdb83166e47c52a9c47ea02f73ff37b8d1791ae7
DeboraAlice7 <32077412+deboraalice7@users.noreply.github.com>
2017-11-07 11:37:43 -0300

    Update heap.h

--- CP_FP_vs_HP/heap.h
+++ CP_FP_vs_HP/heap.h
@@ -19,9 +19,9 @@ extern "C" {
 typedef struct Heap Heap;
 
 /*
- * FunÃ§Ã£o: createHeap
+ * Função: createHeap
  * ----------------------------
- *   capacity: inteiro de capacidade mÃ¡xima
+ *   capacity: inteiro de capacidade máxima
  *   heapType: char com o tipo da heap
  *
  *   retorna: Heap* de uma heap criada.
@@ -29,21 +29,21 @@ typedef struct Heap Heap;
 Heap* createHeap(int capacity, char heapType);
 
 /*
- * Function: enqueueHeap
+ * Função: enqueueHeap
  * ----------------------------
  *   heap: Heap* para ser enfileirada
  *   item: inteiro a ser infilerado na heap 
  *
- *  retorna: um inteiro de comparaÃ§Ãµes
+ *  retorna: um inteiro de comparações
  *   
  */
 int enqueueHeap(Heap *heap, int item);
 
 /*
- * Function: getParentIndex
+ * Função: getParentIndex
  * ----------------------------
  *   heap: Heap* para pegar o parente
- *   i: inteiro para pega o Ã­ndice
+ *   i: inteiro para pega o índice
  *
  *  retorna: um parente inteiro
  *  
@@ -51,68 +51,68 @@ int enqueueHeap(Heap *heap, int item);
 int getParentIndex(Heap *heap, int i);
 
 /*
- * Function: getLeftIndex
+ * Função: getLeftIndex
  * ----------------------------
  *   heap: Heap* para pegar o parente
- *   i: inteiro para pega o Ã­ndice
+ *   i: inteiro para pega o índice
  *
- *  retorna: um parente inteiro do Ã­ndice esquerdo
+ *  retorna: um parente inteiro do índice esquerdo
  *
  */
 int getLeftIndex(Heap *heap, int i);
 
 /*
- * Function: getRightIndex
+ * Função: getRightIndex
  * ----------------------------
  *    heap: Heap* para pegar o parente
- *    i: inteiro para pega o Ã­ndice
+ *    i: inteiro para pega o índice
  *
- *   retorna: um parente inteiro do Ã­ndice direito
+ *   retorna: um parente inteiro do índice direito
  */
 int getRightIndex(Heap *heap, int i);
 
 /*
- * Function: frontHeap
+ * Função: frontHeap
  * ----------------------------
- *   heap: Heap* para pegar a cabeÃ§a
+ *   heap: Heap* para pegar a cabeça
  *
- *   retorna: um inteiro que Ã© a cabeÃ§a
+ *   retorna: um inteiro que é a cabeça
  */
 int frontHeap(Heap *heap);
 
 /*
- * Function: isHeapEmpty
+ * Função: isHeapEmpty
  * ----------------------------
  *   heap: Heap* a ser checada
  *
- *   retorna: verdadeiro se vazia, e falso se nÃ£o estiver
+ *   retorna: verdadeiro se vazia, e falso se não estiver
  * 
  */
 bool isHeapEmpty(Heap *heap);
 
 /*
- * Function: heapify
+ * Função: heapify
  * ----------------------------
  *   heap: Heap* para criar a heapify
- *   i: inteiro onde comeÃ§a a heapify
- *   compares: inteiro com o nÃºmero de comparaÃ§Ãµes 
+ *   i: inteiro onde começa a heapify
+ *   compares: inteiro com o número de comparações 
  *   
  *   retorna: nada
  */
 void heapify(Heap *heap, int i, int *compares);
 
 /*
- * Function: itemOf
+ * Função: itemOf
  * ----------------------------
  *   heap: Heap* a ser pega 
- *   i: Ã­ndice inteiro a ser pego 
+ *   i: índice inteiro a ser pego 
  *   
  *   retorna: um item inteiro
  */
 int itemOf(Heap *heap, int i);
 
 /*
- * Function: heapSort
+ * Função: heapSort
  * ----------------------------
  *   heap: Heap* a ser ordenada
  *   
@@ -121,7 +121,7 @@ int itemOf(Heap *heap, int i);
 void heapSort(Heap *heap);
 
 /*
- * Function: dequeueHeap
+ * Função: dequeueHeap
  * ----------------------------
  *   heap: Heap* a ser desenfilerada 
  *   
@@ -130,9 +130,9 @@ void heapSort(Heap *heap);
 int dequeueHeap(Heap *heap);
 
 /*
- * Function: destroyHeap
+ * Função: destroyHeap
  * ----------------------------
- *   heap: Heap* para liberar a memÃ³ria   
+ *   heap: Heap* para liberar a memória   
  *   
  *   retorna: nada
  */
