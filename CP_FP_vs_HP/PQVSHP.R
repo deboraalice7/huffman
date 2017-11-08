ddefcf00350c792e58c20ba31984523a7c93a58e
DeboraAlice7 <32077412+deboraalice7@users.noreply.github.com>
2017-11-07 23:23:59 -0300

    Add files via upload

--- CP_FP_vs_HP/PQVSHP.R
+++ CP_FP_vs_HP/PQVSHP.R
@@ -1,14 +1,17 @@
 data <- read.csv("priorityQueue_vs_heap.csv", header=TRUE)
+color <- rainbow(130, start = 0.4, end = 1)
+plot(data$PQ, type="p", xaxt = "n", col=color, ann=FALSE, pch = 16, cex = .2)
+compl <- loess(data$PQ~data$SEARCHES)
+lines(data$SEARCHES, predict(compl), col = "red", lwd = 10)
 
-plot(data$PQ, type="o", xaxt = "n", col="red", ann=FALSE)
-
-axis(1, at=1:50, data$SEARCHES)
+axis(1, at=1:10000, data$SEARCHES)
 box()
+
 
 lines(data$HP, type="o", col="blue")
 
 title(main="Priority Queue vs Heap", col.main="blue", font.main=4)
-title(xlab="Valores buscados", col.lab=rgb(0,0.5,0))
+title(xlab="Itens", col.lab=rgb(0,0.5,0))
 title(ylab="Comparações", col.lab=rgb(0,0.5,0))
 
-legend(1, 20, c("PriorityQueue","Heap"), lty=c(1,1), lwd=c(2.5,2.5),col=c("red","blue"))
+legend(1, 6000, c("PriorityQueue","Heap"), lty=c(1,1), lwd=c(2.5,2.5),col=c("red","blue"))
